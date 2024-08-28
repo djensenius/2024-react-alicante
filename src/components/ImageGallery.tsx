4f2dadcc75dfdf784d58781f2bef34196fc5b1d0
David Jensenius <david@jensenius.com>
2024-08-28 19:40:36 -0400

    Add images to gallery

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -1,21 +1,30 @@
 import React from 'react';
+import image1 from '../images/IMG_3137.jpg';
+import image2 from '../images/IMG_3538.jpg';
+import image3 from '../images/Eras-8.jpg';
+import image4 from '../images/IMG_3591.jpg';
 
 const ImageGallery: React.FC = () => {
   const images = [
     {
-      src: 'https://via.placeholder.com/150',
+      src: image1,
       title: 'Image 1',
       description: 'Description for Image 1',
     },
     {
-      src: 'https://via.placeholder.com/150',
+      src: image2,
       title: 'Image 2',
       description: 'Description for Image 2',
     },
     {
-      src: 'https://via.placeholder.com/150',
+      src: image3,
       title: 'Image 3',
       description: 'Description for Image 3',
+    },
+    {
+      src: image4,
+      title: 'Image 4',
+      description: 'Description for Image 4',
     },
   ];
 
