17c80410fcb58a89ddc57dd2f31b9423a08368f4
David Jensenius <david@jensenius.com>
2024-09-11 01:30:25 +0000

    Nav fix, and image wrapping

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,22 +3,22 @@ import { Link } from 'react-router-dom';
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="bg-gray-100 p-4">
+    <nav className="bg-blue-600 p-4 shadow-lg">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="text-white text-lg font-bold">
-          <Link to="/">Dachshund information</Link>
+        <div className="text-white text-2xl font-bold">
+          <Link to="/">Dachshund Info</Link>
         </div>
         <div className="space-x-4">
-          <Link to="/" className="text-gray-300 hover:text-white">
+          <Link to="/" className="text-white hover:text-gray-300 transition duration-300">
             Home
           </Link>
-          <Link to="/gallery" className="text-gray-300 hover:text-white">
+          <Link to="/gallery" className="text-white hover:text-gray-300 transition duration-300">
             Gallery
           </Link>
-          <Link to="/about" className="text-gray-300 hover:text-white">
+          <Link to="/about" className="text-white hover:text-gray-300 transition duration-300">
             About
           </Link>
-          <Link to="/contact" className="text-gray-300 hover:text-white">
+          <Link to="/contact" className="text-white hover:text-gray-300 transition duration-300">
             Contact
           </Link>
         </div>
