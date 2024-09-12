551f36a9b26044438d21a6e28cb67161b8ee6e14
David Jensenius <david@jensenius.com>
2024-09-12 18:58:18 -0400

    More cypress configuration

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,16 +1,13 @@
-describe('settings page', () => {
 beforeEach(() => {
   cy.visit('http://localhost:3000');
   cy.injectAxe();
 });
 
-// Basic usage
 it('Has no detectable a11y violations on load', () => {
   // Test the page at initial load
   cy.checkA11y();
 });
 
-// Applying a context and run parameters
 it('Has no detectable a11y violations on load (with custom parameters)', () => {
   // Test the page at initial load (with context and options)
   cy.checkA11y('.example-class', {
@@ -19,5 +16,4 @@ it('Has no detectable a11y violations on load (with custom parameters)', () => {
       values: ['wcag2a'],
     },
   });
-});
 });
